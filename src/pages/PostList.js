36497955225612ce180135718d8e14d21bb28ab9
尤辉 <youhui@souche.com>
2019-05-19 14:08:08 +0800

    feat: 添加form的功能

--- src/pages/PostList.js
+++ src/pages/PostList.js
@@ -4,28 +4,40 @@ import ItemList from './components/ItemList'
 
 
 class PostList extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             list: []
         };
-        this._handelVote = this._handelVote.bind(this)
+        this._handelVote = this._handelVote.bind(this);
+        this._handleSave = this._handleSave.bind(this)
     }
     
     componentDidMount() {
         axios.get('https://www.easy-mock.com/mock/5cdd1e719bcfaf0b145465a1/test/list')
-        .then((res)=>{
+        .then((res) => {
             const list = res.data.data.posts;
             this.setState({list})
         })
     }
     
-    _handelVote(id){
-        const newList = this.state.list.map((item)=>{
+    _handelVote(id) {
+        const newList = this.state.list.map((item) => {
             return item.id === id ? {...item, vote: ++item.vote} : item
         });
         this.setState({
             list: newList
+        })
+    }
+    
+    _handleSave(post) {
+        // 根据post的ID，过滤出当前要更新的post
+        const posts = this.state.list.map(item => {
+            return item.id === post.id ? post : item
+        });
+        
+        this.setState({
+            list: posts
         })
     }
     
@@ -35,8 +47,8 @@ class PostList extends Component {
                 <div>帖子列表：</div>
                 <ul>
                     {
-                        this.state.list.map((item)=>{
-                            return <ItemList _handelVote={this._handelVote} key={item.id} item={item}/>
+                        this.state.list.map((item) => {
+                            return <ItemList onSave={this._handleSave} _handelVote={this._handelVote} key={item.id} item={item}/>
                         })
                     }
                 </ul>
--- src/pages/components/ItemList.js
+++ src/pages/components/ItemList.js
@@ -1,17 +1,77 @@
 import React, {Component} from 'react'
 
 class ItemList extends Component {
-    handelVote = () =>{
+    constructor(props) {
+        super(props);
+        this.state = {
+            editing: false,  // 控制帖子
+            post: props.item
+        }
+    }
+    
+    componentWillReceiveProps(nextProps, nextContext) {
+        // 父组件更新post后，更新子组件的state
+        if (this.props.post !== nextProps.item) {
+            this.setState({
+                post: nextProps.item
+            })
+        }
+    }
+    
+    handelVote = () => {
         this.props._handelVote(this.props.item.id)
+    };
+    handelTitleChange = (e) => {
+        const newPost = { ...this.state.post, title: e.target.value };
+        this.setState({
+            post: newPost
+        })
+    };
+    
+    // 保存/编辑按钮点击逻辑
+    handleEditPost = () => {
+        const editing = this.state.editing;
+        
+        if (editing) {
+            this.props.onSave({
+                ...this.state.post
+            })
+        }
+        
+        this.setState({
+            editing: !editing
+        })
     };
     
     render() {
-        const {title,vote} = this.props.item;
+        const {post} = this.state;
         return (
             <li>
-                <div>{title}</div>
                 <div>
-                    <button onClick={this.handelVote}>点赞</button><span>{vote}</span>
+                    {
+                        this.state.editing
+                            ? <form>
+                                <textarea
+                                    value={post.title}
+                                    onChange={this.handelTitleChange}
+                                />
+                            </form>
+                            : post.title
+                    }
+                </div>
+                <div>
+                    创建人：<span>{post.author}</span>
+                </div>
+                <div>
+                    创建时间：<span>{post.date}</span>
+                </div>
+                <div>
+                    <button onClick={this.handelVote}>点赞</button><span>{post.vote}</span>
+                </div>
+                <div>
+                    <button onClick={this.handleEditPost}>
+                        {this.state.editing ? '保存' : '编辑'}
+                    </button>
                 </div>
             </li>
         )
