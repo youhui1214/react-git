864b273480bb9698ede47f3d7fb877e473306cb7
尤辉 <youhui@souche.com>
2019-08-02 14:51:34 +0800

    feat: 修改测试上传文件

--- src/pages/PostList.js
+++ src/pages/PostList.js
@@ -21,7 +21,7 @@ class PostList extends Component {
         })
     }
     _handelVote(id) {
-        console.log(20);
+        console.log(2000);
         const newList = this.state.list.map((item) => {
             return item.id === id ? {...item, vote: ++item.vote} : item
         });
